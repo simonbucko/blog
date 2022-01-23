c368c2684b181515403eae9d8efe1b256850a560
SimonBucko <bucino.36@gmail.com>
2022-01-23 19:07:48 +0100

    implemented author

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Author = () => {
-  return <div></div>;
+const Author = ({ author }) => {
+  return (
+    <div>
+      <img
+        src={author.photo.url}
+        alt={author.name}
+        height="100px"
+        width="100px"
+        className="align-middle rounded-full"
+      />
+    </div>
+  );
 };
 
 export default Author;
