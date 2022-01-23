74c49015ee4d37c4ecaf4eb785629d0da366253a
SimonBucko <bucino.36@gmail.com>
2022-01-23 12:14:54 +0100

    added separate components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import {PostCard, Categories, PostWidget} from "../components"
 
 const posts = [
   {
@@ -25,16 +26,14 @@ export default function Home() {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post,index) => (
-          <div>
-            {post.title}
-            {post.excerpt}
-          </div>
+            <PostCard post={post} key={index}/>
           )
          )}
         </div>
         <div className="lg:col-span-4 col-span-1">
             <div className="lg:sticky relative top-8">
-
+              <PostWidget />
+              <Categories/>
             </div>
         </div>
       </div>
