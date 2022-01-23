f1a2501db376a43ca6bc9dabb51d2884d67a073d
SimonBucko <bucino.36@gmail.com>
2022-01-23 09:19:25 +0100

    finish backbone for index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,43 @@
 import Head from 'next/head'
 
+const posts = [
+  {
+    title:"React testing",
+    excerpt: "Learn react testing"
+  },
+  {
+    title:"React testing2",
+    excerpt: "Learn react testing"
+  },
+  {
+    title:"React testing3",
+    excerpt: "Learn react testing"
+  },
+]
+
 export default function Home() {
   return (
-    <div className="container mx=auto px=10 mb-8">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>Create Next App</title>
+        <title>My Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post,index) => (
+          <div>
+            {post.title}
+            {post.excerpt}
+          </div>
+          )
+         )}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+            <div className="lg:sticky relative top-8">
 
-      
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
