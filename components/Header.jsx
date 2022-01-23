464df0e0f89cfe3468f02511f4faee9b2b6fdf23
SimonBucko <bucino.36@gmail.com>
2022-01-23 12:55:19 +0100

    added header

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
       <div className="border-b w-full inline-block border-blue-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">
+            <span className="cursor-pointer font-bold text-4xl text-black">
               Simon's Blog
             </span>
           </Link>
@@ -26,7 +26,7 @@ const Header = () => {
         <div className="hidden md:float-left md:contents">
           {categories.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+              <span className="md:float-right mt-2 align-middle text-black ml-4 font-semibold cursor-pointer">
                 {category.name}
               </span>
             </Link>
