cdb14296c0f77239c3d30768a73301722a4b086e
SimonBucko <bucino.36@gmail.com>
2022-01-23 16:42:52 +0100

    exported components

---
+++ components/CommentsForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CommentsForm = () => {
+  return <div>comments form</div>;
+};
+
+export default CommentsForm;
---
+++ components/PostDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostDetail = () => {
+  return <div>postdetail</div>;
+};
+
+export default PostDetail;
