5ce919c2f55561e5c29eeec1d18a7fff2c7b2570
SimonBucko <bucino.36@gmail.com>
2022-01-24 08:28:52 +0100

    correct api call

--- services/index.js
+++ services/index.js
@@ -149,22 +149,26 @@ export const getComments = async (slug) => {
     return result.comments
 }
 
-export const getRecentPosts = async () => {
+export const getFeaturedPosts = async () => {
     const query = gql`
-      query GetPostDetails() {
-        posts(
-          orderBy: createdAt_ASC
-          last: 3
-        ) {
-          title
+      query GetCategoryPost() {
+        posts(where: {featuredPost: true}) {
+          author {
+            name
+            photo {
+              url
+            }
+          }
           featuredImage {
             url
           }
-          createdAt
+          title
           slug
+          createdAt
         }
-      }
+      }   
     `;
+
     const result = await request(graphqlAPI, query);
 
     return result.posts;
